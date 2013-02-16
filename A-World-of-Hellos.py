b72af453facfe96d887aeb26960a97ba770a8e6e
iczesmv <iczesmv@gmail.com>
2013-02-15 17:43:05 -0700

    Added script

---
+++ A-World-of-Hellos.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  A-World-of-Hellos.py
+#  
+#  Copyright 2013 Jonathan <jonathan@jonathan-Lenovo-G560>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+hellos = {"english":"Hello, World!", "spanish":"\u00a1Hola mundo!", "french":"Salut le Monde!"}
+
+def main():
+	while True:
+		lang = input('In what language do you want your Hello World?:')
+		
+		if lang == "quit":
+			break
+		
+		hello = hellos[lang]
+		print(hello)
+	
+	return 0
+
+if __name__ == '__main__': 
+	main()
